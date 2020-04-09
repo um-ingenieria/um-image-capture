3d98a0cd7258ce953b489cbf8fa81d5574939adb
SEBASTIAN LUCIANO GONZALEZ <sebastianlgonzalez@unimoron.edu.ar>
2020-04-09 13:11:35 -0700

    initial

---
+++ ProyectoCapturaDePantalla/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ProyectoCapturaDePantalla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxImg = new System.Windows.Forms.PictureBox();
+            this.timerCaptura = new System.Windows.Forms.Timer(this.components);
+            this.timerLapso = new System.Windows.Forms.Timer(this.components);
+            this.buttonEmpezar = new System.Windows.Forms.Button();
+            this.buttonTerminar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxWebCam = new System.Windows.Forms.PictureBox();
+            this.comboBoxWebCam = new System.Windows.Forms.ComboBox();
+            this.comboBoxPantallas = new System.Windows.Forms.ComboBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.buttonRunSP = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImg
+            // 
+            this.pictureBoxImg.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxImg.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxImg.Name = "pictureBoxImg";
+            this.pictureBoxImg.Size = new System.Drawing.Size(324, 235);
+            this.pictureBoxImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxImg.TabIndex = 0;
+            this.pictureBoxImg.TabStop = false;
+            // 
+            // timerCaptura
+            // 
+            this.timerCaptura.Tick += new System.EventHandler(this.timerCaptura_Tick);
+            // 
+            // timerLapso
+            // 
+            this.timerLapso.Enabled = true;
+            this.timerLapso.Tick += new System.EventHandler(this.timerLapso_Tick);
+            // 
+            // buttonEmpezar
+            // 
+            this.buttonEmpezar.Location = new System.Drawing.Point(10, 310);
+            this.buttonEmpezar.Name = "buttonEmpezar";
+            this.buttonEmpezar.Size = new System.Drawing.Size(152, 66);
+            this.buttonEmpezar.TabIndex = 5;
+            this.buttonEmpezar.Text = "Empezar";
+            this.buttonEmpezar.UseVisualStyleBackColor = true;
+            this.buttonEmpezar.Click += new System.EventHandler(this.buttonEmpezar_Click);
+            // 
+            // buttonTerminar
+            // 
+            this.buttonTerminar.Location = new System.Drawing.Point(189, 310);
+            this.buttonTerminar.Name = "buttonTerminar";
+            this.buttonTerminar.Size = new System.Drawing.Size(147, 67);
+            this.buttonTerminar.TabIndex = 6;
+            this.buttonTerminar.Text = "Terminar";
+            this.buttonTerminar.UseVisualStyleBackColor = true;
+            this.buttonTerminar.Click += new System.EventHandler(this.buttonTerminar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pictureBoxWebCam);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(712, 264);
+            this.panel1.TabIndex = 9;
+            // 
+            // pictureBoxWebCam
+            // 
+            this.pictureBoxWebCam.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxWebCam.Location = new System.Drawing.Point(373, 13);
+            this.pictureBoxWebCam.Name = "pictureBoxWebCam";
+            this.pictureBoxWebCam.Size = new System.Drawing.Size(324, 235);
+            this.pictureBoxWebCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxWebCam.TabIndex = 10;
+            this.pictureBoxWebCam.TabStop = false;
+            // 
+            // comboBoxWebCam
+            // 
+            this.comboBoxWebCam.FormattingEnabled = true;
+            this.comboBoxWebCam.Location = new System.Drawing.Point(555, 276);
+            this.comboBoxWebCam.Name = "comboBoxWebCam";
+            this.comboBoxWebCam.Size = new System.Drawing.Size(137, 21);
+            this.comboBoxWebCam.TabIndex = 10;
+            this.comboBoxWebCam.SelectedIndexChanged += new System.EventHandler(this.comboBoxWebCam_SelectedIndexChanged);
+            // 
+            // comboBoxPantallas
+            // 
+            this.comboBoxPantallas.FormattingEnabled = true;
+            this.comboBoxPantallas.Location = new System.Drawing.Point(382, 276);
+            this.comboBoxPantallas.Name = "comboBoxPantallas";
+            this.comboBoxPantallas.Size = new System.Drawing.Size(137, 21);
+            this.comboBoxPantallas.TabIndex = 11;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(168, 276);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(169, 20);
+            this.textBoxName.TabIndex = 12;
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(12, 279);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(150, 13);
+            this.labelNombre.TabIndex = 13;
+            this.labelNombre.Text = "Inserte el nombre de la prueba";
+            // 
+            // buttonRunSP
+            // 
+            this.buttonRunSP.Location = new System.Drawing.Point(587, 310);
+            this.buttonRunSP.Name = "buttonRunSP";
+            this.buttonRunSP.Size = new System.Drawing.Size(104, 66);
+            this.buttonRunSP.TabIndex = 14;
+            this.buttonRunSP.Text = "Correr proceso de carga";
+            this.buttonRunSP.UseVisualStyleBackColor = true;
+            this.buttonRunSP.Click += new System.EventHandler(this.buttonRunSP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 389);
+            this.Controls.Add(this.buttonRunSP);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.comboBoxPantallas);
+            this.Controls.Add(this.comboBoxWebCam);
+            this.Controls.Add(this.buttonTerminar);
+            this.Controls.Add(this.buttonEmpezar);
+            this.Controls.Add(this.pictureBoxImg);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImg;
+        private System.Windows.Forms.Timer timerCaptura;
+        private System.Windows.Forms.Timer timerLapso;
+        private System.Windows.Forms.Button buttonEmpezar;
+        private System.Windows.Forms.Button buttonTerminar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxWebCam;
+        private System.Windows.Forms.ComboBox comboBoxWebCam;
+        private System.Windows.Forms.ComboBox comboBoxPantallas;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Button buttonRunSP;
+    }
+}
+
---
+++ ProyectoCapturaDePantalla/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+using System.IO;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace ProyectoCapturaDePantalla
+{
+    public partial class Form1 : Form
+    {
+        /*CAMBIAR EL SQLCONNECTION */
+       // SqlConnection Conexion = new SqlConnection("Data Source=(localdb)\\ServidorSqlGonzalo;Initial Catalog=UM_TESIS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlConnection Conexion = new SqlConnection("Data Source=DESKTOP-63P1QPG;Initial Catalog=UM_NEUROSKY;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+       
+        string Directorio = "C:\\xampp\\htdocs\\imagenes\\public\\imagen";
+       
+
+        
+        SqlCommand cmd;
+
+        int ValorExcitacion = 0;
+        int ValorValencia = 0;
+
+
+        int contador = 0;
+        int LapsoDeTiempo = 0;
+
+        bool arranco;
+
+        string NombrePrueba;
+        int Seccion;
+        int Identificador = 0;
+
+        long TiempoCaptura;
+        String ImagenDirectorioEscritorio;
+        String ImagenDirectorioWebcam;
+
+
+        AForge.Video.DirectShow.VideoCaptureDevice VideoSource;
+        AForge.Video.DirectShow.FilterInfoCollection VideoSources;
+
+        Screen[] screens2;
+
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timerLapso.Stop();
+            timerCaptura.Stop();
+           
+            buttonEmpezar.Focus();
+            
+
+
+            VideoSources = new AForge.Video.DirectShow.FilterInfoCollection(AForge.Video.DirectShow.FilterCategory.VideoInputDevice);
+            if (VideoSources != null)
+            {
+                foreach(Screen screens1 in Screen.AllScreens)
+                {
+                    comboBoxPantallas.Items.Add(screens1.DeviceName);
+                }
+
+                screens2 = Screen.AllScreens;
+
+                comboBoxPantallas.SelectedIndex = 0;
+
+                foreach (FilterInfo VideoSource in VideoSources)
+                {
+                    comboBoxWebCam.Items.Add(VideoSource.Name);
+                }
+
+                
+
+                VideoSource = new AForge.Video.DirectShow.VideoCaptureDevice(VideoSources[0].MonikerString);
+                VideoSource.NewFrame +=  new NewFrameEventHandler(FinalFrame_NewFrame); 
+                VideoSource.Start();
+            }
+            
+
+            arranco = false;
+
+            buttonTerminar.Enabled = false;
+
+            Conexion.Open();
+
+            SqlCommand cmd = new SqlCommand("select max(SECCION) AS SECCION from [NEUROSKY_IMAGENES]", Conexion);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                Int32.TryParse(Convert.ToString(dr["SECCION"]), out this.Seccion);
+                Seccion++;
+            }
+            Conexion.Close();
+        }
+
+
+        
+
+        private void buttonEmpezar_Click(object sender, EventArgs e)
+        {
+     
+            if (textBoxName.Text == "" || textBoxName.Text == "0")
+            {
+                MessageBox.Show("Inserte un nombre antes de empezar la prueba");
+            }
+            else
+            {
+                
+
+                
+                Pulsador ExcitacionValencia = new Pulsador();
+                ExcitacionValencia.Text = "Excitación/Valencia";
+                ExcitacionValencia.ShowDialog();
+
+                this.ValorExcitacion = ExcitacionValencia.DarValorExcitacion();
+                this.ValorValencia = ExcitacionValencia.DarValorValencia();
+
+                if (this.ValorExcitacion == 99 || this.ValorValencia == 99)
+                    MessageBox.Show("Algo salio mal, cierre la ejecucion y vuelva a intentarlo");
+                else
+                {
+
+
+
+                    NombrePrueba = textBoxName.Text;
+
+
+                    this.Insert_Excitacion_Valencia(NombrePrueba, "Al iniciar", ValorExcitacion, ValorValencia);
+                    MessageBox.Show("Excitación: " + this.ValorExcitacion + " / Valencia: " + this.ValorValencia);
+
+                    LapsoDeTiempo = 1000;
+                    this.timerLapso.Interval = LapsoDeTiempo;
+                    buttonTerminar.Enabled = true;
+                    comboBoxPantallas.Enabled = false;
+                    comboBoxWebCam.Enabled = false;
+                    buttonEmpezar.Enabled = false;
+                    timerLapso.Start();
+
+                }
+
+            }
+        }
+
+
+        private void timerLapso_Tick(object sender, EventArgs e)
+        {
+            this.buttonCapturar_Click(sender, e);
+            timerLapso.Stop();
+            timerLapso.Start();
+           
+        }
+
+
+
+        private void buttonCapturar_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+
+            this.timerCaptura.Start();
+
+
+        }
+
+        private void timerCaptura_Tick(object sender, EventArgs e)
+        {
+            if (contador == 1)
+            {
+                arranco = true;
+
+               
+
+                this.Identificador++;
+                
+                Bitmap Imgb = new Bitmap(screens2[comboBoxPantallas.SelectedIndex].WorkingArea.Width, screens2[comboBoxPantallas.SelectedIndex].WorkingArea.Height, PixelFormat.Format32bppArgb);
+                Graphics graf = Graphics.FromImage(Imgb);
+                
+                graf.CopyFromScreen(screens2[comboBoxPantallas.SelectedIndex].WorkingArea.X, screens2[comboBoxPantallas.SelectedIndex].WorkingArea.Y, 0, 0, screens2[comboBoxPantallas.SelectedIndex].WorkingArea.Size, CopyPixelOperation.SourceCopy);
+                
+                pictureBoxImg.Image = Imgb;
+
+                TiempoCaptura = (long)DateTime.UtcNow.Ticks;
+
+                ImagenDirectorioEscritorio = String.Format(@"{0}\Escritorio-{1}.jpg", Directorio, TiempoCaptura);
+                Imgb.Save(ImagenDirectorioEscritorio, ImageFormat.Bmp);
+
+                ImagenDirectorioWebcam = String.Format(@"{0}\WebCam-{1}.jpg", Directorio, TiempoCaptura);
+                pictureBoxWebCam.Image.Save(ImagenDirectorioWebcam, System.Drawing.Imaging.ImageFormat.Png);
+
+
+                
+
+
+                Conexion.Open();
+                string SqlQuery = "INSERT INTO NEUROSKY_IMAGENES (NOMBREPRUEBA,SECCION,IDENTIFICADOR,DATE,IMAGENESCRITORIO,IMAGENWEBCAM)VALUES('" + this.NombrePrueba + "'," + this.Seccion + "," + this.Identificador + ",'" + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "','" + ImagenDirectorioEscritorio + "', '" + ImagenDirectorioWebcam + "')";
+                cmd = new SqlCommand(SqlQuery, Conexion);
+                
+
+                int N = cmd.ExecuteNonQuery();
+                Conexion.Close();
+
+
+                
+                timerCaptura.Stop();
+            }
+            contador++;
+        }
+
+
+
+        private void CerrarSeccion()
+        {
+            if (arranco == true)
+            {
+                Seccion++;
+                Identificador = 0;
+
+            }
+            arranco = false;
+        }
+
+        private void buttonTerminar_Click(object sender, EventArgs e)
+        {
+            timerLapso.Stop();
+            //this.CallSP();
+            this.FinalizarCapturas();
+
+        }
+
+
+        public int CallSP(string SP)
+        {
+            string NombreQuery;
+            int resultado;
+            if (NombrePrueba == null)
+                NombreQuery = textBoxName.Text;
+            else
+                NombreQuery = NombrePrueba;
+
+
+            Conexion.Open();
+            SqlCommand cmd = new SqlCommand(SP, Conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = NombreQuery;
+            resultado = cmd.ExecuteNonQuery();
+            Conexion.Close();
+
+            return resultado;
+        }
+        
+
+        public void FinalizarCapturas()
+        {
+            if (textBoxName.Text == "" || textBoxName.Text == "0")
+            {
+                MessageBox.Show("Inserte un nombre antes de empezar la prueba");
+            }
+            else
+            {
+                Pulsador ExcitacionValencia = new Pulsador();
+                ExcitacionValencia.Text = "Excitación/Valencia";
+                ExcitacionValencia.ShowDialog();
+
+                this.ValorExcitacion = ExcitacionValencia.DarValorExcitacion();
+                this.ValorValencia = ExcitacionValencia.DarValorValencia();
+
+                if (this.ValorExcitacion == 99 || this.ValorValencia == 99)
+                    MessageBox.Show("Algo salio mal, cierre la ejecucion y vuelva a intentarlo");
+                else
+                {
+
+
+
+                    NombrePrueba = textBoxName.Text;
+
+
+                    this.Insert_Excitacion_Valencia(NombrePrueba, "Al Finalizar", ValorExcitacion, ValorValencia);
+                    MessageBox.Show("Excitación: " + this.ValorExcitacion + " / Valencia: " + this.ValorValencia);
+
+                    this.CerrarSeccion();
+
+                    buttonTerminar.Enabled = false;
+                    buttonEmpezar.Enabled = true;
+                    comboBoxPantallas.Enabled = true;
+                    comboBoxWebCam.Enabled = true;
+                    buttonEmpezar.Focus();
+
+                }
+            }
+        }
+
+
+
+        
+       private void comboBoxWebCam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (VideoSources != null)
+            {
+                if (VideoSource.IsRunning)
+                    VideoSource.Stop();
+                VideoSource = new AForge.Video.DirectShow.VideoCaptureDevice(VideoSources[comboBoxWebCam.SelectedIndex].MonikerString);
+                VideoSource.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+                VideoSource.Start();
+            }
+        }
+
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBoxWebCam.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (VideoSource.IsRunning == true)
+                VideoSource.Stop();
+        }
+
+        private void buttonRunSP_Click(object sender, EventArgs e)
+        {
+            
+            //this.CallSP();
+            MessageBox.Show("El numero de registros afectados en la tabla general fueron: " + this.CallSP("SP_CargarDatos"));
+            MessageBox.Show("El numero de registros afectados en la tabla Excitacion-Valencia fueron: " + this.CallSP("SP_CargarDatos_Excitacion_Valencia"));
+        }
+
+
+        private void Insert_Excitacion_Valencia(string name,string periodicidad, int Excitacion, int valencia)
+        {
+
+            Conexion.Open();
+            string SqlQuery1 = "INSERT INTO Excitacion_VALENCIA (NAME_TEST,Seccion,TIPO,EXCITACION,VALENCIA) VALUES('" + name + "',"+ this.Seccion + ",'" + periodicidad + "'," + Excitacion + ", NULL)";
+            cmd = new SqlCommand(SqlQuery1, Conexion);
+            int N1 = cmd.ExecuteNonQuery();
+            Conexion.Close();
+
+            Conexion.Open();
+            string SqlQuery2 = "INSERT INTO Excitacion_VALENCIA (NAME_TEST,Seccion,TIPO,EXCITACION,VALENCIA) VALUES('" + name + "'," + this.Seccion + ",'" + periodicidad + "', NULL ," + valencia + ")";
+            cmd = new SqlCommand(SqlQuery2, Conexion);
+            int N2 = cmd.ExecuteNonQuery();
+            Conexion.Close();
+        }
+    }
+
+}
