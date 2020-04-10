a65032e7b7842df82e8e3bf73d0ead787ffccd05
Sebastian Gonzalez <sebastiangon11@gmail.com>
2020-04-10 15:09:59 -0700

    call emotion api

--- ProyectoCapturaDePantalla/face/FaceService.cs
+++ ProyectoCapturaDePantalla/face/FaceService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 namespace ProyectoCapturaDePantalla.face
 {
@@ -10,7 +12,121 @@ namespace ProyectoCapturaDePantalla.face
     {
         public FaceService()
         {
+            string subscriptionKey = "968cb12edb324b7b87678753e6537f03";
+            string cognitiveEndpoint = "https://emotion-recognition-api.cognitiveservices.azure.com/";
+
             Console.WriteLine("Servicio creado, no soy un singleton");
+
+            // Authenticate.
+            IFaceClient client = Authenticate(cognitiveEndpoint, subscriptionKey);
+
+            detectFaces(client);
         }
+
+        private async Task detectFaces(IFaceClient client)
+        {
+            await DetectFaceExtract(client, "https://csdx.blob.core.windows.net/resources/Face/Images/", RecognitionModel.Recognition01);
+        }
+
+        public static IFaceClient Authenticate(string endpoint, string key)
+        {
+            return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+
+        public static async Task DetectFaceExtract(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========DETECT FACES========");
+            Console.WriteLine();
+
+            // Create a list of images
+            List<string> imageFileNames = new List<string>
+                    {
+                        "https://gutenberg.rocks/wp-content/uploads/2018/07/mujer-feliz.jpg"
+                    };
+
+            foreach (var imageFileName in imageFileNames)
+            {
+                IList<DetectedFace> detectedFaces;
+
+                // Detect faces with all attributes from image url.
+                detectedFaces = await client.Face.DetectWithUrlAsync($"{imageFileName}",
+                returnFaceLandmarks: true,
+                returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+                FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+                FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+                FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile,},
+                recognitionModel: recognitionModel);
+
+                Console.WriteLine($"{detectedFaces.Count} face(s) detected from image `{imageFileName}`.");
+
+                // Parse and print all attributes of each detected face.
+                foreach (var face in detectedFaces)
+                {
+                    Console.WriteLine($"Face attributes for {imageFileName}:");
+
+                    // Get bounding box of the faces
+                    Console.WriteLine($"Rectangle(Left/Top/Width/Height) : {face.FaceRectangle.Left} {face.FaceRectangle.Top} {face.FaceRectangle.Width} {face.FaceRectangle.Height}");
+
+                    // Get accessories of the faces
+                    List<Accessory> accessoriesList = (List<Accessory>)face.FaceAttributes.Accessories;
+                    int count = face.FaceAttributes.Accessories.Count;
+                    string accessory; string[] accessoryArray = new string[count];
+                    if (count == 0) { accessory = "NoAccessories"; }
+                    else
+                    {
+                        for (int i = 0; i < count; ++i) { accessoryArray[i] = accessoriesList[i].Type.ToString(); }
+                        accessory = string.Join(",", accessoryArray);
+                    }
+                    Console.WriteLine($"Accessories : {accessory}");
+
+                    // Get face other attributes
+                    Console.WriteLine($"Age : {face.FaceAttributes.Age}");
+                    Console.WriteLine($"Blur : {face.FaceAttributes.Blur.BlurLevel}");
+
+                    // Get emotion on the face
+                    string emotionType = string.Empty;
+                    double emotionValue = 0.0;
+                    Emotion emotion = face.FaceAttributes.Emotion;
+                    if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
+                    if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
+                    if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
+                    if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
+                    if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
+                    if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
+                    if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
+                    if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
+                    Console.WriteLine($"Emotion : {emotionType}");
+
+                    // Get more face attributes
+                    Console.WriteLine($"Exposure : {face.FaceAttributes.Exposure.ExposureLevel}");
+                    Console.WriteLine($"FacialHair : {string.Format("{0}", face.FaceAttributes.FacialHair.Moustache + face.FaceAttributes.FacialHair.Beard + face.FaceAttributes.FacialHair.Sideburns > 0 ? "Yes" : "No")}");
+                    Console.WriteLine($"Gender : {face.FaceAttributes.Gender}");
+                    Console.WriteLine($"Glasses : {face.FaceAttributes.Glasses}");
+
+                    // Get hair color
+                    Hair hair = face.FaceAttributes.Hair;
+                    string color = null;
+                    if (hair.HairColor.Count == 0) { if (hair.Invisible) { color = "Invisible"; } else { color = "Bald"; } }
+                    HairColorType returnColor = HairColorType.Unknown;
+                    double maxConfidence = 0.0f;
+                    foreach (HairColor hairColor in hair.HairColor)
+                    {
+                        if (hairColor.Confidence <= maxConfidence) { continue; }
+                        maxConfidence = hairColor.Confidence; returnColor = hairColor.Color; color = returnColor.ToString();
+                    }
+                    Console.WriteLine($"Hair : {color}");
+
+                    // Get more attributes
+                    Console.WriteLine($"HeadPose : {string.Format("Pitch: {0}, Roll: {1}, Yaw: {2}", Math.Round(face.FaceAttributes.HeadPose.Pitch, 2), Math.Round(face.FaceAttributes.HeadPose.Roll, 2), Math.Round(face.FaceAttributes.HeadPose.Yaw, 2))}");
+                    Console.WriteLine($"Makeup : {string.Format("{0}", (face.FaceAttributes.Makeup.EyeMakeup || face.FaceAttributes.Makeup.LipMakeup) ? "Yes" : "No")}");
+                    Console.WriteLine($"Noise : {face.FaceAttributes.Noise.NoiseLevel}");
+                    Console.WriteLine($"Occlusion : {string.Format("EyeOccluded: {0}", face.FaceAttributes.Occlusion.EyeOccluded ? "Yes" : "No")} " +
+                        $" {string.Format("ForeheadOccluded: {0}", face.FaceAttributes.Occlusion.ForeheadOccluded ? "Yes" : "No")}   {string.Format("MouthOccluded: {0}", face.FaceAttributes.Occlusion.MouthOccluded ? "Yes" : "No")}");
+                    Console.WriteLine($"Smile : {face.FaceAttributes.Smile}");
+                    Console.WriteLine();
+                }
+            }
+        }
+
     }
 }
