b411876811b16d5f5e7278037de792a162edbe0b
Sebastian <sebastiangon11@gmail.com>
2020-04-19 18:05:57 -0300

    add loading and prompt forms

---
+++ ProyectoCapturaDePantalla/LoadingView.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ProyectoCapturaDePantalla
+{
+    partial class LoadingView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProcessing = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblProcessing
+            // 
+            this.lblProcessing.AutoSize = true;
+            this.lblProcessing.Location = new System.Drawing.Point(98, 35);
+            this.lblProcessing.Name = "lblProcessing";
+            this.lblProcessing.Size = new System.Drawing.Size(73, 13);
+            this.lblProcessing.TabIndex = 0;
+            this.lblProcessing.Text = "Procesando...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 65);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(246, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // LoadingView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(270, 116);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblProcessing);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingView";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoadingView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProcessing;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ ProyectoCapturaDePantalla/Prompt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCapturaDePantalla
+{
+    public class Prompt
+    {
+        Form propmtForm;
+        Label label;
+        TextBox textBox;
+        Button confirmation;
+
+        public Prompt(string caption, string text)
+        {
+            this.propmtForm = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            this.label = new Label() { Left = 50, Top = 20, Text = text };
+            this.textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            this.confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { propmtForm.Close(); };
+            propmtForm.Controls.Add(textBox);
+            propmtForm.Controls.Add(confirmation);
+            propmtForm.Controls.Add(label);
+            propmtForm.AcceptButton = confirmation;
+        }
+
+        public string show()
+        {
+            return propmtForm.ShowDialog() == DialogResult.OK ? this.textBox.Text : "";
+        }
+    }
+}
