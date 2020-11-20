a485479250aa694b89b0f8d3a3baf78fc38d711c
seba <sebastiangon11@gmail.com>
2020-11-19 21:33:45 -0300

    create phases copadas

---
+++ SQL/Create phases-3.sql
@@ -0,0 +1,146 @@
+--HAPV 1
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8501);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8492);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8490);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 4659);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8400);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8370);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 4290);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8341);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 8179);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'IAP_TYPE', 'HA_PV', 4668);
+
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 37.1);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 55.2);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 49.1);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 14.2);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 28.1);
+INSERT INTO PHASE VALUES (9, 'High Arousal - Positive Valence', 'Fase 9', 'DEVO_TYPE', 'HA_PV', 36.5);
+
+--LAPV-1
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 5020);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 1450);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 5726);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 1670);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 1610);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 2501);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 5760);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 5811);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 1604);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'IAP_TYPE', 'LA_PV', 2037);
+
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'DEVO_TYPE', 'LA_PV', 102.4);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'DEVO_TYPE', 'LA_PV', 102.5);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'DEVO_TYPE', 'LA_PV', 131.1);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'DEVO_TYPE', 'LA_PV', 21.6);
+INSERT INTO PHASE VALUES (10, 'Low Arousal - Positive Valence', 'Fase 10', 'DEVO_TYPE', 'LA_PV', 17.5);
+
+-- LA NV
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9331);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9220);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 2750);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9291);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9832);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9342);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 2205);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 3300);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 9830);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'IAP_TYPE', 'LA_NV', 2900.1);
+
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'DEVO_TYPE', 'LA_NV', 135.2);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'DEVO_TYPE', 'LA_NV', 7.8);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'DEVO_TYPE', 'LA_NV', 110.2);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'DEVO_TYPE', 'LA_NV', 137.1);
+INSERT INTO PHASE VALUES (11, 'Low Arousal - Negative Valence', 'Fase 11', 'DEVO_TYPE', 'LA_NV', 5.3);
+
+--HA NV
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 6350);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 1120);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 3000);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 9413);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 2730);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 3010);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 9908);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 9940);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 3170);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'IAP_TYPE', 'HA_NV', 3080);
+
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'DEVO_TYPE', 'HA_NV', 88.1);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'DEVO_TYPE', 'HA_NV', 95.1);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'DEVO_TYPE', 'HA_NV', 96.1);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'DEVO_TYPE', 'HA_NV', 80.2);
+INSERT INTO PHASE VALUES (12, 'High Arousal - Negative Valence', 'Fase 12', 'DEVO_TYPE', 'HA_NV', 110.1);
+
+--HAPV 2
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 4698);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 8185);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 8034);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 4810);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 5629);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 4800);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 4681);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 8160);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 5621);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'IAP_TYPE', 'HA_PV', 4670);
+
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'DEVO_TYPE', 'HA_PV', 12.1);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'DEVO_TYPE', 'HA_PV', 18.1);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'DEVO_TYPE', 'HA_PV', 54.2);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'DEVO_TYPE', 'HA_PV', 33.1);
+INSERT INTO PHASE VALUES (13, 'High Arousal - Positive Valence', 'Fase 13', 'DEVO_TYPE', 'HA_PV', 36.2);
+
+--LAPV-2
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5030);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 2320);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5010);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5711);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 2270);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 1333);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5891);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5875);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 5410);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'IAP_TYPE', 'LA_PV', 1910);
+
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 102.2);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 29.2);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 56.5);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 112.2);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 21.3);
+INSERT INTO PHASE VALUES (14, 'Low Arousal - Positive Valence', 'Fase 14', 'DEVO_TYPE', 'LA_PV', 9.3);
+
+-- LA NV
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9041);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9561);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9415);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9432);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9320);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 6311);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 2722);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 9001);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 2590);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'IAP_TYPE', 'LA_NV', 2490);
+
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'DEVO_TYPE', 'LA_NV', 65.3);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'DEVO_TYPE', 'LA_NV', 115.2);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'DEVO_TYPE', 'LA_NV', 117.2);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'DEVO_TYPE', 'LA_NV', 134.1);
+INSERT INTO PHASE VALUES (15, 'Low Arousal - Negative Valence', 'Fase 15', 'DEVO_TYPE', 'LA_NV', 47.2);
+
+--HA NV
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 6550);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 1050);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 3060);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 1931);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 9410);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 3053);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 1300);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 6563);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 3130);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'IAP_TYPE', 'HA_NV', 3213);
+
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'DEVO_TYPE', 'HA_NV', 66.3);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'DEVO_TYPE', 'HA_NV', 90.1);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'DEVO_TYPE', 'HA_NV', 93.3);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'DEVO_TYPE', 'HA_NV', 82.2);
+INSERT INTO PHASE VALUES (16, 'High Arousal - Negative Valence', 'Fase 16', 'DEVO_TYPE', 'HA_NV', 78.1);
+
